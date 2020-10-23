ecda5b56bfc45ff25b7b1fff842612c524bfd73e
Bryan Wu <bryanwu@berkeley.edu>
2020-10-22 19:40:16 -0700

    complete most of the tag command

--- src/cli.js
+++ src/cli.js
@@ -1,1 +1,33 @@
-const commander = require('commander');
+const { Command } = require('commander');
+const mongoose = require('mongoose');
+const dbUrl = process.env.MONGO_URL || 'mongodb://localhost/gradtrak';   
+const CONFIG = {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+};
+const connect = async () => {
+  await mongoose.connect(dbUrl, CONFIG);
+
+  console.log('MongoDB connected');
+
+  return mongoose.connection;
+};
+const Course = require('../models/course');
+
+
+module.exports.program = program = new Command();
+
+program.name('gradtrak');
+
+program.command('tag <tag-id> <course-ids...>').action(async(tagId, courseIds) => {
+  await connect();
+  let courses = await Course.find({}, { _id: 0, __v: 0 })
+  courses = courses.filter(course => {
+    return courseIds.includes(course.id);
+  })
+  courses.forEach(course => {
+    course.tagIds.push(tagId);
+    course.save();
+    console.log(`tagged ${course.id} with ${tagId}`)
+  })
+})
